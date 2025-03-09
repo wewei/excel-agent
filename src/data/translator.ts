820960cb00b2b357a0343bcfd763c66a20fc7e1a
Wei Wei <shazhouke@gmail.com>
2025-03-09 18:19:18 +0800

    Add tinyLisp

--- src/data/translator.ts
+++ src/data/translator.ts
@@ -1,22 +1,24 @@
 import { SheetData } from "./sheetData";
+import { createList, createMap, createMultiArityDefn, createAtom, createSymbol, generateCode } from "./tinyLisp";
 
 // Translate the sheetData into Lisp code (in Clojure)
 export function toLisp(sheetData: SheetData): string {
-  // 将单元格数据转换为 Clojure map 格式
-  const cellsToClojure = sheetData.cells.map((cell) => {
-    // 对值进行转义,处理特殊字符
-    const escapedValue = cell.value
-      .replace(/\\/g, "\\\\")
-      .replace(/"/g, '\\"')
-      .replace(/\n/g, "\\n");
-
-    return `  {:row ${cell.row}, :col ${cell.col}, :value "${escapedValue}"}`;
-  });
-
-  // 构建格式化的 Clojure 代码
-  const clojureCode = `(def cells
-    [${cellsToClojure.length ? "\n" + cellsToClojure.join("\n") + "\n]" : "])"}
-
-    (def sheet {:cells cells})`;
-  return clojureCode;
+  return generateCode(
+    createMultiArityDefn("sheet", [
+      {
+        params: ["cells"],
+        body: [
+          createList(
+            sheetData.cells.map((cell) =>
+              createMap([
+                [createSymbol("row"), createAtom(cell.row, "number")],
+                [createSymbol("col"), createAtom(cell.col, "number")],
+                [createSymbol("value"), createAtom(cell.value, "string")],
+              ])
+            )
+          ),
+        ],
+      },
+    ])
+  );
 }
