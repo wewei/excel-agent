69ba02f2a5a0fbae762846b1e421c5978d8952d7
Wei Wei <shazhouke@gmail.com>
2025-03-09 21:58:04 +0800

    Add the fast-formula-parser

--- src/data/runner.ts
+++ src/data/runner.ts
@@ -4,3 +4,6 @@ import { exec } from "@jcubic/lips";
 export function runCode(code: string): Promise<any> {
   return exec(code);
 }
+
+// @ts-ignore
+window.sheet = () => { console.log("sheet"); return "sheet" }
--- src/data/translator.ts
+++ src/data/translator.ts
@@ -1,6 +1,6 @@
 import { SheetData } from "./sheetData";
 
-// Translate the sheetData into Lisp code (in Clojure)
+// Translate the sheetData into Lisp code (in Scheme)
 export function toLisp(sheetData: SheetData): string {
   return `(sheet ${sheetData.cells.map(cell => `(cell ${cell.row} ${cell.col} "${cell.value}")`).join(" ")})`;
 }
